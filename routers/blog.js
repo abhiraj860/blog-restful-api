a0ed048b50d14103b797eb4af2be224ba8d20784
Abhiraj Aditya <abhiaditya860@gmail.com>
2023-12-30 16:46:01 +0530

    getallpost-filterpostbyid-done

--- routers/blog.js
+++ routers/blog.js
@@ -5,7 +5,7 @@ const {User} = require('../db/models');
 const {Blog} = require('../db/models');
 const jwt = require('jsonwebtoken');
 
-
+// Create blog
 router.post('/', userMiddleware, async (req, res)=>{
     const username = req.username;
     const find = await User.findOne({username});
@@ -29,8 +29,27 @@ router.post('/', userMiddleware, async (req, res)=>{
         blog
     });
 });
-// router.post('/:blogid', userMiddleware, (req, res)=>{
 
-// });
+// Get all blogposts or by tags
+router.get('/', userMiddleware, async (req, res)=>{
+    const getTags = req.query.tags;
+    if(getTags) {
+        const find = await Blog.find({
+            tags: {$in: [getTags]}
+        });
+        if(find.length === 0) return res.status(500).json({
+            msg: "No such tags exits"
+        });
+        return res.status(200).json({
+            blogs:find
+        });
+    }    
+    const blogs = await Blog.find({});
+    res.status(200).json({
+        blogs
+    });
+});
+
+
 
 module.exports = router;
